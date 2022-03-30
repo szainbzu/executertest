d2e2215ee4be78e71fe1c0bb6816a9921ffd1c1a
szainbzu <szain@birzeit.edu>
2022-03-30 19:57:52 +0200

    commit version 1

---
+++ app/src/main/java/edu/cs/birzeit/executertest/MainActivity.java
@@ -0,0 +1,47 @@
+package edu.cs.birzeit.executertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btn_click(View view) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String str = "done";
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //UI Thread work here
+                        Toast.makeText(MainActivity.this,
+                                str, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+}
